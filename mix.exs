2af07999287507b2447a81e1f725da924bdc9c63
metehan <metehanarslan@gmail.com>
2025-09-04 11:36:45 +0200

    description and package name

--- mix.exs
+++ mix.exs
@@ -7,6 +7,13 @@ defmodule ElixirWebTerminal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description:
+        "A minimal Elixir application that serves a real terminal in the browser using Plug and Cowboy.",
+      package: [
+        name: "exterm",
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/metehan/exterm"}
+      ],
       deps: deps()
     ]
   end
